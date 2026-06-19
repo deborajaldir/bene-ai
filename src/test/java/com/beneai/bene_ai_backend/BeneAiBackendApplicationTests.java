4c0d536d354dca89d9efffbc12566884be08bd7e
debora-jaldir <deborajaldir@gmail.com>
2026-06-19 11:41:23 -0300

    chore: initial backend setup with Spring Boot and PostgreSQL

---
+++ src/test/java/com/beneai/bene_ai_backend/BeneAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beneai.bene_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
